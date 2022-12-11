c7fdf7fa197ac128493e575979d875876f8176f1
lilxak <zakben45@gmail.com>
2022-12-11 18:08:27 +0100

    Adapt ExamAdminForm to multi categories courses

--- courses/forms.py
+++ courses/forms.py
@@ -1,8 +1,9 @@
 ''' Courses app's forms '''
 
+from typing import List
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Category, Exam
+from .models import Category, Exam, Course
 
 
 class SearchForm(forms.Form):
@@ -17,13 +18,13 @@ class ExamAdminForm(forms.ModelForm):
         fields = '__all__'
 
     def clean(self):
-        related_courses = self.cleaned_data.get('related_courses')
+        related_courses: List[Course] = self.cleaned_data.get('related_courses')
         categories = self.cleaned_data.get('categories')
 
         if not related_courses and not categories:
             raise ValidationError('You must either specify related courses or categories')
         for course in related_courses:
-            category = Category.objects.filter(id=course.category.id)
+            category = Category.objects.filter(id__in=course.categories.all())
             self.cleaned_data['categories'] = self.cleaned_data.get(
                 'categories').union(category)
 
