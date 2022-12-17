be62f60737d884ee40c0ac0928e03c87087c2ffb
Zaki <33980130+benaissazaki@users.noreply.github.com>
2022-12-17 17:00:01 +0100

    Style pages (#9)
    * Style index page
    
    * Style course-list and exam-list views
    
    * Style course-detail and exam-detail

--- courses/forms.py
+++ courses/forms.py
@@ -8,7 +8,8 @@ from .models import Category, Exam, Course
 
 class SearchForm(forms.Form):
     ''' Form to search for courses '''
-    query = forms.CharField(label='Query', max_length=100)
+    query = forms.CharField(label='Query',
+                            max_length=100)
 
 
 class ExamAdminForm(forms.ModelForm):
@@ -18,11 +19,13 @@ class ExamAdminForm(forms.ModelForm):
         fields = '__all__'
 
     def clean(self):
-        related_courses: List[Course] = self.cleaned_data.get('related_courses')
+        related_courses: List[Course] = self.cleaned_data.get(
+            'related_courses')
         categories = self.cleaned_data.get('categories')
 
         if not related_courses and not categories:
-            raise ValidationError('You must either specify related courses or categories')
+            raise ValidationError(
+                'You must either specify related courses or categories')
         for course in related_courses:
             category = Category.objects.filter(id__in=course.categories.all())
             self.cleaned_data['categories'] = self.cleaned_data.get(
